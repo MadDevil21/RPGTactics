f57ffaa60dff2366d784647e5ae2ba33e1b653c7
Martelo <martelo300@hotmail.com>
2018-03-26 20:51:22 +0100

    created class Constants, Grid and Cells are being drawn

---
+++ src/main/test/java/graphics_test/GridTest.java
@@ -0,0 +1,13 @@
+package graphics_test;
+
+import org.academiadecodigo.haltistas.graphics.Grid;
+
+public class GridTest {
+
+    public static void main(String[] args) {
+
+        Grid grid = new Grid();
+        grid.init();
+
+    }
+}
