c089e3708e818412faca1352fd4b719b558abb18
Martelo <martelo300@hotmail.com>
2018-03-27 19:12:29 +0100

    drawing character in random pos between a defined Row and col

--- src/main/java/org/academiadecodigo/haltistas/graphics/Cell.java
+++ src/main/java/org/academiadecodigo/haltistas/graphics/Cell.java
@@ -6,19 +6,14 @@ import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 public class Cell implements Collidable{
 
-    private Rectangle cell;
-    private int col;
-    private int row;
     private boolean collidable;
 
 
     public Cell(int col, int row) {
 
-        this.col = col;
-        this.row = row;
         this.collidable = false;
 
-        cell = new Rectangle(Constants.PADDING + (col * Constants.CELL_SIZE), Constants.PADDING +
+        Rectangle cell = new Rectangle(Constants.PADDING + (col * Constants.CELL_SIZE), Constants.PADDING +
                 (row * Constants.CELL_SIZE), Constants.CELL_SIZE, Constants.CELL_SIZE);
 
         cell.draw();
